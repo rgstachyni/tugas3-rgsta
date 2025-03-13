45cc73534954041f26432eaeecd88f2cb2dc5d84
rgstachyni <registacahyani303@gmail.com>
2025-03-13 22:35:40 +0700

    Add files via upload

--- about.php
+++ about.php
@@ -7,7 +7,7 @@
     <div class="container-fluid">
         <h1 class="mt-4 text-center">Tentang Saya</h1>
         <img src="img/bot.png" alt="" class="w-25 d-block mx-auto rounded-circle border border-5 mt-2">
-        <p class="text-center offset-2 col-8 mt-4 fs-5">Hallo, nama saya Muhammad Syamil. Saya mahasiswa jurusan Sistem Informasi di STT Nurul Fikri. Saat ini saya berusia 19 tahun dan tinggal di Ciawi, Bogor. Saya tertarik dengan dunia teknologi, khususnya dalam pengembangan sistem dan analisis data. Saya juga suka belajar hal baru dan senang mencari solusi kreatif dalam pemrograman. Senang bisa berkenalan!</p>
+        <p class="text-center offset-2 col-8 mt-4 fs-5">Hallo, nama saya Regista Cahyani. Saya mahasiswa jurusan Sistem Informasi di STT Nurul Fikri. Saat ini saya berusia 19 tahun dan tinggal di Juanda, Depok. Saya tertarik dengan dunia teknologi, khususnya dalam pengembangan sistem dan analisis data. Saya juga suka belajar hal baru dan senang mencari solusi kreatif dalam pemrograman. Senang bisa berkenalan!</p>
     </div>
 </div>
 <?php include_once "template/bottom.php";?>
